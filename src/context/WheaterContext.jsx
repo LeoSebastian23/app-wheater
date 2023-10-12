0f9a2a6a71c8cfcd7ca853ed0cb7f1dc4e9b97f6
Leonardo Sebastian Gauto <112297703+LeoSebastian23@users.noreply.github.com>
2023-10-12 19:01:55 -0300

    initial commit

---
+++ src/context/WheaterContext.jsx
@@ -0,0 +1,40 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+
+const WeatherContext = createContext();
+
+export function useWeatherContext() {
+  return useContext(WeatherContext);
+}
+
+export function WeatherProvider({ children }) {
+  const [cityName, setCityName] = useState('Mar del plata');
+  const [inputText, setInputText] = useState('');
+  const [data, setData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
+      )
+        .then((res) => {
+          if (res.status === 200) {
+            error && setError(false);
+            return res.json();
+          } else {
+            throw new Error("Something went wrong");
+          }
+        })
+        .then((data) => {
+          setData(data);
+        })
+        .catch(() => setError(true))
+        .finally(() => setLoading(false));  
+  }, [cityName, error]);
+
+  return (
+    <WeatherContext.Provider value={{ cityName, setCityName, inputText, setInputText, data, error, loading }}>
+      {children}
+    </WeatherContext.Provider>
+  );
+}
