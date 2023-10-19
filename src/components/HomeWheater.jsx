a7bdf8dc31e9c045b5a66f91cc571c083fdb0b1d
Leonardo Sebastian Gauto <112297703+LeoSebastian23@users.noreply.github.com>
2023-10-19 03:42:05 -0300

    add styles

--- src/components/HomeWheater.jsx
+++ src/components/HomeWheater.jsx
@@ -4,30 +4,34 @@ import "../App.css";
 import { WiHumidity, WiCloudyGusts, WiThermometer } from "react-icons/wi";
 
 const containerStyles = {
-  maxWidth: "70%", // Establece el ancho máximo del contenedor
+  maxWidth: "80%", // Establece el ancho máximo del contenedor
   margin: "0 auto", // Centra el contenedor en el medio
 };
 
 const HomeWeather = ({ data, loading }) => {
   return (
     <div className="bg_img" style={containerStyles}>
-      <div className="bg-home">
-        <h1 className="city">{data.name}</h1>
+      <div className="bg-01">
+        <div className="city">{data.name}</div>
+        <div className="temp">{data.main.temp.toFixed()} °C</div>
         <div className="group">
           <img
             src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
             alt=""
+            className="weather-icon"
           />
-          <h1>{data.weather[0].main}</h1>
+          <div className="weather-description">{data.weather[0].main}</div>
         </div>
-        </div>
-      <div className="cardHome" style={{background:'#537EC5'}}>
-        <h1 className="temp">{data.main.temp.toFixed()} °C</h1>
-
+      </div>
+      <div className="card-today">
         <div className="temperature-min-max">
-          <h2>Min: {data.main.temp_min.toFixed()} °C</h2>
-          <h2 style={{ padding: "5px" }}>-</h2>
-          <h2>Max: {data.main.temp_max.toFixed()} °C</h2>
+          <h2 className="min-max-temperature">
+            Min: {data.main.temp_min.toFixed()} °C
+          </h2>
+          <h2 className="divider">-</h2>
+          <h2 className="min-max-temperature">
+            Max: {data.main.temp_max.toFixed()} °C
+          </h2>
         </div>
       </div>
       <Slide direction="right" timeout={800} in={!loading}>
